b159792d2654965d996dfc07eb86bf332de563c2
Jayendra <jayendra06mishra@gmail.com>
2026-05-26 02:07:50 +0530

    Add basic note creation functionality

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Notes App Started");
+const noteInput = document.getElementById("noteInput");
+const addBtn = document.getElementById("addBtn");
+const notesList = document.getElementById("notesList");
+
+addBtn.addEventListener("click", addNote);
+
+function addNote() {
+
+    const noteText = noteInput.value.trim();
+
+    if (noteText === "") {
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.textContent = noteText;
+
+    notesList.appendChild(li);
+
+    noteInput.value = "";
+}
